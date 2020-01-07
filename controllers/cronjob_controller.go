438f7d7c061780afc2bea2159dcfbc9fac4d39ae
Wlodek Borkowski <wlodek.borkowski@gmail.com>
2020-01-07 13:41:19 +0100

    delete unsued imports

--- controllers/cronjob_controller.go
+++ controllers/cronjob_controller.go
@@ -25,7 +25,6 @@ import (
 	"github.com/robfig/cron"
 	kbatch "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ref "k8s.io/client-go/tools/reference"
